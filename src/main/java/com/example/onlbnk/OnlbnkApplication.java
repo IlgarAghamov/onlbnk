0e5356fce41f71285736af724b2d51e6126b63f6
Ilgar Aghamov <ilgar.aghamov@gmail.com>
2022-11-11 17:44:17 +0400

    Initial commit

---
+++ src/main/java/com/example/onlbnk/OnlbnkApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlbnk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlbnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlbnkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlbnk/OnlbnkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlbnk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlbnkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
